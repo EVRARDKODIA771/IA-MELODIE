ba8a1510b0c0840aa0effe27d03fe1ef7242d5b8
EVRARDKODIA771 <kodia.evrard@outlook.com>
2026-01-15 18:05:35 +0000

    Create fingerprint.py

---
+++ melody/fingerprint.py
@@ -0,0 +1,9 @@
+# melody/fingerprint.py
+
+def pitch_to_fingerprint(pitches):
+    """
+    Transforme une liste de pitches en "fingerprint" pour comparaison.
+    Pour l'instant, retourne la même liste.
+    """
+    # TODO : remplacer par vrai calcul de fingerprint
+    return pitches
