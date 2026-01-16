04f3f076a6e5231b8c359714c9368217887df652
EVRARDKODIA771 <kodia.evrard@outlook.com>
2026-01-16 23:57:12 +0000

    Update App.jsx

--- Frontend/App.jsx
+++ Frontend/App.jsx
@@ -1,4 +1,18 @@
 import Recorder from "./Recorder.jsx";
+import { useEffect } from "react";
+useEffect(async() => {
+  const pingBackend = () => {
+    fetch("https://ia-melodie.onrender.com/ping").catch(() => {});
+  };
+
+  // Ping toutes les 5 minutes
+  const interval = setInterval(pingBackend, 5 * 60 * 1000);
+
+  // Ping immédiat au chargement
+  await pingBackend();
+
+  return () => clearInterval(interval);
+}, []);
 
 function App() {
   return (
